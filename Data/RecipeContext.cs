fa29e1b1d2b2fbc31fd5865483f961eddbcbbd1e
Konrad Milewski <konradmilewski2000@interia.eu>
2021-05-23 18:57:43 +0200

    Wstępny model oraz DBcontext

---
+++ Data/RecipeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrzepisyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrzepisyWeb.Data
+{
+    public class RecipeContext : IdentityUserContext<ApplicationUser>
+    {
+        public RecipeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrzepisyWeb.Models
+{
+    public class Recipe
+    {
+        //params
+
+        protected int id;
+        private string Name;
+        private string Ingredients;
+        private string Description;
+        private DateTime Date;
+
+        //Owner
+        private IdentityUser user;
+
+        //Constructors
+
+        public Recipe(string Name, DateTime Date, string Description, string Ingredients)
+        {
+            setName(Name);
+            setDate(Date);
+            setDescription(Description);
+            setIngredients(Ingredients);
+
+        }
+
+
+        //setters
+
+        public void setName(string Name)
+        {
+            this.Name = Name;
+        }
+        public void setDate(DateTime Time)
+        {
+            this.Date = Time;
+        }
+        public void setDescription(string Description)
+        {
+            this.Description = Description;
+        }
+        public void setIngredients(string Ingredients)
+        {
+            this.Ingredients = Ingredients;
+        }
+
+        //getters
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public DateTime getDate()
+        {
+            return this.Date;
+        }
+        public string getDescription()
+        {
+            return this.Description;
+        }
+        public string getIngredients()
+        {
+            return this.Ingredients;
+        }
+        public int getID()
+        {
+            return this.id;
+        }
+
+        public IdentityUser getUser()
+        {
+            return this.user;
+        }
+    }
+}
