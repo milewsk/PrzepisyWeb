779a01407d6a28f29a74005f9ee2f337e8de2919
Gabriela Malinowska <pysia24.20@onet.pl>
2021-06-14 17:08:59 +0200

    zmiany z prod + front z prod-gabrysia

---
+++ Migrations/20210614150602_spr.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrzepisyWeb.Migrations
+{
+    public partial class spr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
